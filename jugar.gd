905ba8f15e41eb4a5ab18b2075048ad69bee1f4d
abel hv <115476506+abelHV@users.noreply.github.com>
2024-11-15 18:44:39 +0100

    ButtonExit

--- jugar.gd
+++ jugar.gd
@@ -1,11 +1,14 @@
 extends Button
 
 
-
-
-
-	
-
-
 func _on_jugar_pressed():
 	get_tree().change_scene("res://Juego.tscn") # Replace with function body.
+
+
+
+
+
+
+
+func _on_salir_pressed():
+	get_tree().quit(); # Replace with function body.
