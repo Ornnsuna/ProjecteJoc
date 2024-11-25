c295bc5d881c425fbced7348f07f0c067a28ac76
abel hv <115476506+abelHV@users.noreply.github.com>
2024-11-25 20:20:13 +0100

    afegim node2 casellas

--- Juego.gd
+++ Juego.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
-var countdown = 10  
-onready var sprite = $Sprite 
-onready var timer = $Sprite/Timer 
+var countdown = 15  
+onready var sprite = $reglas
+onready var timer = $reglas/Timer 
+onready var mapa = $mapa
+const NUM_CASILLAS = 57
+var casillas = []
+
+
 
 func _ready():
-
 	timer.connect("timeout", self, "_on_timer_timeout")
 	timer.start()
+	mapa.pause_mode = Node.PAUSE_MODE_STOP
+	for i in range(NUM_CASILLAS):
+		casillas[i]=i;
+		
+	
+	 
+
 
 func _on_timer_timeout():
 
@@ -15,3 +26,5 @@ func _on_timer_timeout():
 	if countdown <= 0:
 		sprite.visible = false  
 		timer.stop()
+		mapa.pause_mode = Node.PAUSE_MODE_PROCESS
+
