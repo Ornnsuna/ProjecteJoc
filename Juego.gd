b43fadbdcf4f71ed71889fdfdfd44d06902657ee
abel hv <115476506+abelHV@users.noreply.github.com>
2024-12-11 19:02:35 +0100

    intentoCambioMapa

--- Juego.gd
+++ Juego.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var countdown = 15  
+var countdown = 1  
 onready var sprite = $reglas
 onready var timer = $reglas/Timer 
 onready var mapa = $mapa
@@ -12,11 +12,11 @@ var monedasJug1 = 0
 var monedasJug2 = 0
 var estatsJug1 = [3, -2, -1]
 var estatsJug2 = [1, 1, 1]
-var contPosada = 2
-var contJaula = 2
-var contLaberinto = 3
 var pausaJug1 = false
 var pausaJug2 = false
+var contPausa1 = 0
+var contPausa2 = 0
+
 
 
 
@@ -77,27 +77,27 @@ func _roll_dice(jugador_id):
 	print("Jugador", jugador_id, "tiró", movimiento)
 
 	# Mover al jugador
-	if jugador_id == 1:
-		posJug1 = _mover_jugador(posJug1, movimiento, jugador1, 1)
+	
+	if jugador_id == 1:		
+		posJug1 = _mover_jugador(posJug1, movimiento, jugador1, 1)	
 	elif jugador_id == 2:
 		posJug2 = _mover_jugador(posJug2, movimiento, jugador2, 2)
 
+			
+			
 func _mover_jugador(posicion_actual, movimiento, sprite, id):
 	# Calcular nueva posición
-	if id == 1:
-		$jugador1.self_modulate.a = 0.5
-		$jugador2.self_modulate.a = 1
-		
-	elif id == 2:
-		$jugador2.self_modulate.a = 0.5
-		$jugador1.self_modulate.a = 1
+	
 	var nueva_posicion = posicion_actual + movimiento
+	
 	if nueva_posicion == 17:
 		nueva_posicion = 7
-		print("Me lleva la marea")
+		print("Me lleva la Corriente")
+		
 	elif nueva_posicion == 7:
 		nueva_posicion =17
-		print("Me lleva la marea")
+		print("Me lleva la Corriente")
+		
 	if nueva_posicion == 9 || nueva_posicion == 21 || nueva_posicion == 31 || nueva_posicion == 40 || nueva_posicion ==50:
 		print("¡Oca por oca y tiro por que me toca!")
 		if id == 1:
@@ -106,19 +106,18 @@ func _mover_jugador(posicion_actual, movimiento, sprite, id):
 			$jugador1.self_modulate.a = 1	
 		elif id == 2:
 			turno_jugador = 2
-			$jugador1.self_modulate.a = 0.5
 			$jugador2.self_modulate.a = 1
-	if nueva_posicion == 19: 
+			$jugador1.self_modulate.a = 0.5
+			
+	if nueva_posicion == 19 || nueva_posicion == 29 || nueva_posicion == 44: 
 		if id == 1:
 			pausaJug1 = true
+			contPausa1 = cuenta_Espera(nueva_posicion)
 		elif id == 2:
 			pausaJug2 = true
-	
-	if posicion_actual == 19 && (pausaJug1 == true || pausaJug2 == true ):
-		if id == 1:
-			pausaJug1 = true
-		elif id == 2:
-			pausaJug2 = true
+			contPausa2 = cuenta_Espera(nueva_posicion)
+			
+			
 	if nueva_posicion > NUM_CASILLAS:	
 		retroceder=nueva_posicion - NUM_CASILLAS
 		nueva_posicion = nueva_posicion - retroceder
@@ -135,12 +134,45 @@ func _mover_jugador(posicion_actual, movimiento, sprite, id):
 
 
 func _on_jugador1_pressed():
+	if pausaJug1 == true:
+		print("Jugador 1 está en pausa durante", contPausa1, "turnos restantes.")
+		contPausa1 -= 1
+		if contPausa1 <= 0:
+			pausaJug1 = false
+		turno_jugador = 2  # Pasar el turno al siguiente jugador
+		$jugador1.self_modulate.a = 0.5
+		$jugador2.self_modulate.a = 1
+		return
+
 	if turno_jugador == 1:
-		turno_jugador = 2 # Pasa el turno al jugador 2place with function bod
-		_roll_dice(1)  # Jugador 1 lanza los dados y.
+		turno_jugador = 2
+		$jugador1.self_modulate.a = 0.5
+		$jugador2.self_modulate.a = 1
+		_roll_dice(1)
+		
 
 
 func _on_jugador2_pressed():
+	if pausaJug2 == true:
+		print("Jugador 2 está en pausa durante", contPausa2, "turnos restantes.")
+		contPausa2 -= 1
+		if contPausa2 <= 0:
+			pausaJug2 = false
+		turno_jugador = 1  # Pasar el turno al siguiente jugador
+		$jugador2.self_modulate.a = 0.5
+		$jugador1.self_modulate.a = 1
+		
 	if turno_jugador == 2:
-		turno_jugador = 1  # Pasa el turno al jugador 2place with function body.
-		_roll_dice(2)  # Jugador 1 lanza los dados
+		turno_jugador = 1
+		$jugador2.self_modulate.a = 0.5
+		$jugador1.self_modulate.a = 1
+		_roll_dice(2)
+		
+	
+func cuenta_Espera(nueva_posicion):
+	if nueva_posicion == 19:
+		return 1
+	elif nueva_posicion == 29:
+		return 3
+	elif nueva_posicion == 44:
+		return 2
